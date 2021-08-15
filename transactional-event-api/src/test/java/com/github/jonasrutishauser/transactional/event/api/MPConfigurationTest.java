b0879644a808e4ab80fd1336618694fa8988705d
Jonas Rutishauser <jonas.rutishauser@alumni.ethz.ch>
2021-08-15 16:33:42 +0200

    Add configuration metrics

--- transactional-event-api/src/test/java/com/github/jonasrutishauser/transactional/event/api/MPConfigurationTest.java
+++ transactional-event-api/src/test/java/com/github/jonasrutishauser/transactional/event/api/MPConfigurationTest.java
@@ -12,6 +12,20 @@ class MPConfigurationTest {
     void defaultValues() {
         Configuration testee = new MPConfiguration();
 
+        assertEquals(Configuration.DEFAULT_ALL_IN_USE_INTERVAL, testee.getAllInUseInterval());
+        assertEquals(Configuration.DEFAULT_MAX_DISPATCHER_INTERVAL, testee.getMaxDispatchInterval());
+        assertEquals(Configuration.DEFAULT_MAX_DISPATCHER_INTERVAL / 2, testee.getInitialDispatchInterval());
+        assertEquals(Configuration.DEFAULT_TABLE_NAME, testee.getTableName());
+        assertEquals(Configuration.DEFAULT_MAX_AQUIRE, testee.getMaxAquire());
+        assertEquals(Configuration.DEFAULT_MAX_AQUIRE, testee.getMaxConcurrentDispatching());
+    }
+
+    @Test
+    void configuredAllInUseInterval() {
+        MPConfiguration testee = new MPConfiguration();
+        testee.allInUseInterval = Optional.of(Integer.valueOf(42));
+
+        assertEquals(42, testee.getAllInUseInterval());
         assertEquals(Configuration.DEFAULT_MAX_DISPATCHER_INTERVAL, testee.getMaxDispatchInterval());
         assertEquals(Configuration.DEFAULT_MAX_DISPATCHER_INTERVAL / 2, testee.getInitialDispatchInterval());
         assertEquals(Configuration.DEFAULT_TABLE_NAME, testee.getTableName());
@@ -24,6 +38,7 @@ class MPConfigurationTest {
         MPConfiguration testee = new MPConfiguration();
         testee.maxDispatchInterval = Optional.of(Integer.valueOf(13));
 
+        assertEquals(Configuration.DEFAULT_ALL_IN_USE_INTERVAL, testee.getAllInUseInterval());
         assertEquals(13, testee.getMaxDispatchInterval());
         assertEquals(6, testee.getInitialDispatchInterval());
         assertEquals(Configuration.DEFAULT_TABLE_NAME, testee.getTableName());
@@ -36,6 +51,7 @@ class MPConfigurationTest {
         MPConfiguration testee = new MPConfiguration();
         testee.initialDispatchInterval = Optional.of(Integer.valueOf(42));
 
+        assertEquals(Configuration.DEFAULT_ALL_IN_USE_INTERVAL, testee.getAllInUseInterval());
         assertEquals(Configuration.DEFAULT_MAX_DISPATCHER_INTERVAL, testee.getMaxDispatchInterval());
         assertEquals(42, testee.getInitialDispatchInterval());
         assertEquals(Configuration.DEFAULT_TABLE_NAME, testee.getTableName());
@@ -48,6 +64,7 @@ class MPConfigurationTest {
         MPConfiguration testee = new MPConfiguration();
         testee.tableName = Optional.of("someTable");
 
+        assertEquals(Configuration.DEFAULT_ALL_IN_USE_INTERVAL, testee.getAllInUseInterval());
         assertEquals(Configuration.DEFAULT_MAX_DISPATCHER_INTERVAL, testee.getMaxDispatchInterval());
         assertEquals(Configuration.DEFAULT_MAX_DISPATCHER_INTERVAL / 2, testee.getInitialDispatchInterval());
         assertEquals("someTable", testee.getTableName());
@@ -60,6 +77,7 @@ class MPConfigurationTest {
         MPConfiguration testee = new MPConfiguration();
         testee.maxAquire = Optional.of(Integer.valueOf(7));
 
+        assertEquals(Configuration.DEFAULT_ALL_IN_USE_INTERVAL, testee.getAllInUseInterval());
         assertEquals(Configuration.DEFAULT_MAX_DISPATCHER_INTERVAL, testee.getMaxDispatchInterval());
         assertEquals(Configuration.DEFAULT_MAX_DISPATCHER_INTERVAL / 2, testee.getInitialDispatchInterval());
         assertEquals(Configuration.DEFAULT_TABLE_NAME, testee.getTableName());
@@ -72,6 +90,7 @@ class MPConfigurationTest {
         MPConfiguration testee = new MPConfiguration();
         testee.maxConcurrentDispatching = Optional.of(Integer.valueOf(7));
 
+        assertEquals(Configuration.DEFAULT_ALL_IN_USE_INTERVAL, testee.getAllInUseInterval());
         assertEquals(Configuration.DEFAULT_MAX_DISPATCHER_INTERVAL, testee.getMaxDispatchInterval());
         assertEquals(Configuration.DEFAULT_MAX_DISPATCHER_INTERVAL / 2, testee.getInitialDispatchInterval());
         assertEquals(Configuration.DEFAULT_TABLE_NAME, testee.getTableName());
