a396d39a281ecb625fc17d681825fc8ba7a00689
Jonas Rutishauser <jonas.rutishauser@alumni.ethz.ch>
2021-10-01 16:03:06 +0200

    Avoid WeldException
    Fix `WELD-001524: Unable to load proxy class for bean Configurator Bean [class com.github.jonasrutishauser.transactional.event.core.cdi.EventHandlerExtension, types: EventDeserializer<SomeType>, Object, ExtendedEventDeserializer, qualifiers: @Any @Default] with class class java.lang.Object`

--- transactional-event-core/src/main/java/com/github/jonasrutishauser/transactional/event/core/cdi/ExtendedEventDeserializer.java
+++ transactional-event-core/src/main/java/com/github/jonasrutishauser/transactional/event/core/cdi/ExtendedEventDeserializer.java
@@ -2,6 +2,6 @@ package com.github.jonasrutishauser.transactional.event.core.cdi;
 
 import com.github.jonasrutishauser.transactional.event.api.serialization.EventDeserializer;
 
-interface ExtendedEventDeserializer<T> extends EventDeserializer<T> {
+public interface ExtendedEventDeserializer<T> extends EventDeserializer<T> {
 
 }
